b07114cb93288848c5dab31d2f7d4e49833447aa
Carson Underwood <cunderw@gmail.com>
2024-05-22 15:48:34 -0500

    Add unit tests for mushroom model

---
+++ test/models/mushroom_test.dart
@@ -0,0 +1,84 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:my_mushrooms_hunter/models/mushroom.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:fake_cloud_firestore/fake_cloud_firestore.dart';
+
+void main() {
+  group('Mushroom', () {
+    final mushroom = Mushroom(
+      id: '123',
+      name: 'Amanita',
+      description: 'A poisonous mushroom.',
+      geolocation: LatLng(40.7128, -74.0060),
+      photoUrl: 'http://example.com/photo.jpg',
+      dateFound: DateTime(2023, 5, 22),
+    );
+
+    test('toFirestoreMap should convert Mushroom instance to a Firestore Map',
+        () {
+      final userID = 'user_123';
+      final map = mushroom.toFirestoreMap(userID);
+
+      expect(map['userID'], userID);
+      expect(map['name'], mushroom.name);
+      expect(map['description'], mushroom.description);
+      expect(map['geolocation']['latitude'], mushroom.geolocation.latitude);
+      expect(map['geolocation']['longitude'], mushroom.geolocation.longitude);
+      expect(map['photoUrl'], mushroom.photoUrl);
+      expect((map['dateFound'] as Timestamp).toDate(), mushroom.dateFound);
+    });
+
+    test('fromMap should create a Mushroom instance from a Map', () {
+      final map = {
+        'name': 'Amanita',
+        'description': 'A poisonous mushroom.',
+        'geolocation': {
+          'latitude': 40.7128,
+          'longitude': -74.0060,
+        },
+        'photoUrl': 'http://example.com/photo.jpg',
+        'dateFound': '2023-05-22T00:00:00.000',
+      };
+
+      final newMushroom = Mushroom.fromMap(map);
+
+      expect(newMushroom.name, mushroom.name);
+      expect(newMushroom.description, mushroom.description);
+      expect(newMushroom.geolocation.latitude, mushroom.geolocation.latitude);
+      expect(newMushroom.geolocation.longitude, mushroom.geolocation.longitude);
+      expect(newMushroom.photoUrl, mushroom.photoUrl);
+      expect(newMushroom.dateFound, mushroom.dateFound);
+    });
+
+    test(
+        'fromFirestore should create a Mushroom instance from a Firestore DocumentSnapshot',
+        () async {
+      final firestore = FakeFirebaseFirestore();
+
+      final map = {
+        'name': 'Amanita',
+        'description': 'A poisonous mushroom.',
+        'geolocation': {
+          'latitude': 40.7128,
+          'longitude': -74.0060,
+        },
+        'photoUrl': 'http://example.com/photo.jpg',
+        'dateFound': Timestamp.fromDate(DateTime(2023, 5, 22)),
+      };
+
+      final docRef = await firestore.collection('mushrooms').add(map);
+      final snapshot = await docRef.get();
+
+      final newMushroom = Mushroom.fromFirestore(snapshot);
+
+      expect(newMushroom.id, docRef.id);
+      expect(newMushroom.name, mushroom.name);
+      expect(newMushroom.description, mushroom.description);
+      expect(newMushroom.geolocation.latitude, mushroom.geolocation.latitude);
+      expect(newMushroom.geolocation.longitude, mushroom.geolocation.longitude);
+      expect(newMushroom.photoUrl, mushroom.photoUrl);
+      expect(newMushroom.dateFound, mushroom.dateFound);
+    });
+  });
+}
