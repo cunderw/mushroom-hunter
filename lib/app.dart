2f834f70833cc5496ae5e9ee4372942732c98fa3
Carson Underwood <cunderw@gmail.com>
2024-06-10 10:40:55 -0500

    Add dark mode

--- lib/app.dart
+++ lib/app.dart
@@ -8,8 +8,13 @@ class MyMushroomHunterApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green.shade900),
         useMaterial3: true,
+        colorSchemeSeed: Color.fromRGBO(2, 84, 34, 0.329),
+      ),
+      darkTheme: ThemeData(
+        useMaterial3: true,
+        brightness: Brightness.dark,
+        colorSchemeSeed: Color.fromRGBO(2, 84, 34, 0.329),
       ),
       debugShowCheckedModeBanner: false,
       home: const AuthGate(),
